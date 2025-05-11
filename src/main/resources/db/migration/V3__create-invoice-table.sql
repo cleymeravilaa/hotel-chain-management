0ccea926f900f01f12fd5e58f71e9dbacf35c82c
Cleymer Avila <166467374+cleymeravilaa@users.noreply.github.com>
2025-05-11 14:03:39 +0000

    feat: adding the create tabla invoice script migration

---
+++ src/main/resources/db/migration/V3__create-invoice-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE invoices(
+    invoice_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    staying BIGINT NOT NULL,
+    issueDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    final_total FLOAT NOT NULL
+)
