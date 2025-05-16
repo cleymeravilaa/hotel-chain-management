d68064e8f7b92ec9500b53ec963c0276fcddadfb
Cleymer Avila <166467374+cleymeravilaa@users.noreply.github.com>
2025-05-16 00:45:03 +0000

    feat: handling crud operation for Staying Entity

---
+++ src/main/java/edu/unicolombo/HotelChainManagement/dto/staying/UpdateStayingRoom.java
@@ -0,0 +1,5 @@
+package edu.unicolombo.HotelChainManagement.dto.staying;
+
+public record UpdateStayingRoom() {
+
+}
---
+++ src/main/java/edu/unicolombo/HotelChainManagement/service/StayingService.java
@@ -0,0 +1,76 @@
+package edu.unicolombo.HotelChainManagement.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.unicolombo.HotelChainManagement.domain.model.Room;
+import edu.unicolombo.HotelChainManagement.domain.model.RoomStatus;
+import edu.unicolombo.HotelChainManagement.domain.model.Staying;
+import edu.unicolombo.HotelChainManagement.domain.model.StayingRoom;
+import edu.unicolombo.HotelChainManagement.domain.model.StayingRoomId;
+import edu.unicolombo.HotelChainManagement.domain.repository.BookingRepository;
+import edu.unicolombo.HotelChainManagement.domain.repository.StayingRepository;
+import edu.unicolombo.HotelChainManagement.domain.repository.StayingRoomRepository;
+import edu.unicolombo.HotelChainManagement.dto.staying.StayingDTO;
+import jakarta.transaction.Transactional;
+
+@Service
+public class StayingService {
+
+    @Autowired
+    public BookingRepository bookingRepository;
+
+    @Autowired
+    public StayingRepository stayingRepository;
+
+    @Autowired
+    public StayingRoomRepository StayingRoomRepository;
+
+    @Transactional
+    public StayingDTO registerStaying(Long bookingId) {
+        var booking = bookingRepository.getReferenceById(bookingId);
+
+        Staying staying = new Staying();
+        staying.setBooking(booking);
+        staying.setStartDate(booking.getStartDate());
+        staying.setEndDate(booking.getEndDate());
+        
+        Staying savedStaying = stayingRepository.save(staying);
+
+        List<StayingRoom> stayingRooms = new ArrayList<>();
+        for(Room room: booking.getRooms()){
+            room.setStatus(RoomStatus.OCCUPIED);
+
+            StayingRoomId id = new StayingRoomId(savedStaying.getStayingId(), room.getRoomId());
+
+            StayingRoom stayingRoom = new StayingRoom();
+            stayingRoom.setId(id);
+            stayingRoom.setCheckInDate(staying.getStartDate());
+            stayingRoom.setRoom(room);
+            stayingRoom.setStaying(staying);
+
+            stayingRooms.add(stayingRoom);
+        }
+        savedStaying.setStayingRoom(stayingRooms);
+        
+        StayingRoomRepository.saveAll(stayingRooms);
+        savedStaying = stayingRepository.save(savedStaying);
+
+        return new StayingDTO(savedStaying);
+
+    }
+
+    public List<StayingDTO> getAllStayings(){
+        return stayingRepository.findAll().stream().map(StayingDTO::new).toList();
+    }
+
+    public StayingDTO getStayingById(Long stayingId) {
+        var staying = stayingRepository.getReferenceById(stayingId);
+
+        return new StayingDTO(staying);
+    }
+
+}
